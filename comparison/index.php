a3bda0d4f09f02f2e562a2e48692758c293f0bd3
TylerBlood <tylerlblood@gmail.com>
2019-09-23 16:27:48 -0400

    Completed WEB-230-021 | PHP Comparison Operators

---
+++ comparison/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// part one
+  $number1 = 2;
+  $number2 = 10;
+
+  // != and <> both stand for not equal to
+  if ($number1 <= $number2) {
+    echo 'Yes.';
+  } else {
+    echo 'No.';
+  }
+
+echo '<br/>';
+// part two
+
+  $password = 'password';
+
+  if ($password =='something'){
+    echo 'Correct!';
+  } else {
+    echo 'Incorrect!';
+  }
+
+?>
