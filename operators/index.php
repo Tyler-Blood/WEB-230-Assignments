610a148a969c455f90f8a477b03ff44169b39c94
TylerBlood <tylerlblood@gmail.com>
2019-09-23 16:18:48 -0400

    Completed WEB-230-020 | PHP Assignment Operators

---
+++ operators/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//from first part of video, showing number assignment operators
+$number1 = 10;
+$number1 += 2;
+echo $number1;
+
+echo '<br/>';
+
+// second part of video, text string concatenation
+$text = 'Hello';
+$text .= ' World';
+echo $text;
+
+?>
